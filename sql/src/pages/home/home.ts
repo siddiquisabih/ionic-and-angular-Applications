2df1b7e8b0b85b3f49592f2290d27da15c11d2e3
sabih <sabih@texpo.com>
2018-05-29 16:48:50 +0500

    sql lite

---
+++ sql/src/pages/home/home.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController,private sqlite: SQLite) {
+
+  }
+
+
+
+  createDB() {
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    })
+      .then((db) => {
+    
+    
+        db.executeSql('create table danceMoves(name VARCHAR(32))', {})
+          .then(() => console.log('Executed SQL'))
+          .catch(e => console.log(e));
+    
+    
+      })
+      .catch(e => console.log(e));
+  }
+
+}
