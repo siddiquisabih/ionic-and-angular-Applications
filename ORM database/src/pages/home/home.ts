66a462300b86578c452f3426a74c12deca75bec2
sabih <siddiquisabih94@gmail.com>
2018-07-05 15:12:27 +0500

    orm database

---
+++ ORM database/src/pages/home/home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import "reflect-metadata";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+ 
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
