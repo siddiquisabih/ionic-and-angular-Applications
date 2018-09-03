b53f789be2f8d92db9b3519b7918e08e7cfe4c8d
sabih <siddiquisabih94@gmail.com>
2018-09-03 13:01:12 +0500

    receipt format added

--- ionic Bluetooth Print/src/app/app.component.ts
+++ ionic Bluetooth Print/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { MainPage } from '../pages/main/main';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any = MainPage;
+  rootPage: any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
